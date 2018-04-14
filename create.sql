25d1f872b2441639c6b866a067c8d3a56ad86d59
César Medeiros <cesar.mnm@gmail.com>
2018-04-14 23:08:53 +0100

    UML [Update] Create.sql [Restrictions and bug fix]

--- create.sql
+++ create.sql
@@ -3,34 +3,35 @@ PRAGMA foreign_keys=ON;
 .headers on
 .nullvalue NULL
 
+
 drop table if exists Genre;
 CREATE TABLE Genre(
-	id 	integer PRIMARY KEY,
+	id 		integer PRIMARY KEY,
 	name 	text not null
 );
 
 drop table if exists Language;
 create table Language(
-	id 	integer PRIMARY KEY,
+	id 		integer PRIMARY KEY,
 	name	text not null
 );
 
 drop table if exists Publisher;
 create table Publisher(
-	id	integer PRIMARY KEY,
+	id		integer PRIMARY KEY,
 	name	text not null
 );
 
 drop table if exists Nationality;
 create table Nationality(
-	id	integer PRIMARY KEY,
+	id		integer PRIMARY KEY,
 	name	text not null
 );
 
 drop table if exists Book;
 create table Book(
-	id		integer PRIMARY KEY,
-	ISBN		integer not null,
+	id			integer PRIMARY KEY,
+	ISBN		integer not null UNIQUE,
 	title		text not null,
 	year		integer not null,
 	edition		integer not null,
@@ -54,28 +55,28 @@ create table BookAuthor(
 
 drop table if exists Author;
 create table Author(
-	id		integer PRIMARY KEY,
+	id			integer PRIMARY KEY,
 	name		text not null,
-	birthdate	text not null
+	birthdate	date not null
 );
 
 drop table if exists AuthorNationality;
 create table AuthorNationality(
-	author	integer references Author not null,
+	author		integer references Author not null,
 	nationality integer references Nationality not null,
 	PRIMARY KEY(author, nationality)
 );
 
 drop table if exists Location;
 create table Location(
-	id	integer PRIMARY KEY,
-	pCode	integer not null,
+	id		integer PRIMARY KEY,
+	pCode	integer not null UNIQUE,
 	name	text not null
 );
 
 drop table if exists User;
 create table User(
-	id		integer PRIMARY KEY,
+	id			integer PRIMARY KEY,
 	name		text not null,
 	address		text not null,
 	location	references Location not null
@@ -83,24 +84,24 @@ create table User(
 
 drop table if exists UserNationality;
 create table UserNationality(
-	user	integer references User not null,
-	nationality integer references Language not null,
+	user		integer references User not null,
+	nationality integer references Nationality not null,
 	PRIMARY KEY(user, nationality)
 );
 
 drop table if exists BookItem;
 create table BookItem(
-	id		integer PRIMARY KEY,
-	insertionDate	text not null,
-	book		integer references Book not null,
-	owner		integer references User not null
+	id				integer PRIMARY KEY,
+	insertionDate	date not null,
+	book			integer references Book not null,
+	owner			integer references User not null
 );
-
+	
 drop table if exists Sharing;
 create table Sharing(
-	id		integer PRIMARY KEY,
-	startDate	text not null,
-	endDate		text not null,
+	id			integer PRIMARY KEY,
+	startDate	date not null,
+	endDate		date,
 	book		integer references Book not null,
 	receives	integer references User not null,
 	check(endDate >= startDate)
@@ -108,7 +109,7 @@ create table Sharing(
 
 drop table if exists Request;
 create table Request(
-	id		integer PRIMARY KEY,
+	id			integer PRIMARY KEY,
 	bookTitle	text check(length(bookTitle) <= 40),
 	authorName	text check(length(authorName) <= 40),
 	isFulfilled	integer,
@@ -118,14 +119,10 @@ create table Request(
 
 drop table if exists Message;
 create table Message(
-	id		integer PRIMARY KEY,
-	date		text not null,
+	id			integer PRIMARY KEY,
+	date		date not null,
 	body		text not null check(length(body) <= 500),
 	receives	integer references User not null,
 	senver		integer references User not null,
 	context		integer references BookItem not null
 );
-	
-
-	
-
