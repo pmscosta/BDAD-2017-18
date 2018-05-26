a22be6c706ca92282aa9e4c5abf96dd3ea68c97f
César Medeiros <cesar.mnm@gmail.com>
2018-05-26 22:21:41 +0100

    [Trigger 3]

---
+++ gatilhos/gatilho3_verifica.sql
@@ -0,0 +1,5 @@
+SELECT * FROM SHARING WHERE book = 1;
+
+INSERT INTO Message VALUES(9, '2018-08-20 20:23:03','[Share][Complete]', 2, 5, 1);
+
+SELECT * FROM SHARING WHERE book = 1;
---
+++ queries/int9.sql
@@ -0,0 +1,17 @@
+.mode	columns
+.headers	on
+.nullvalue	NULL
+
+--Autores favoritos
+
+select name
+from
+	(select idAuthor, count(*) as cnt
+	from
+		(select *
+		from Sharing S, BookItem BI, BookAuthor BA
+		where S.book = BI.id and BI.book = BA.idBook)
+		group by idAuthor), Author A
+	where idAuthor = A.id
+order by cnt
+LIMIT 3;
