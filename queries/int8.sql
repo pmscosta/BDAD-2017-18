83ab7aad710a96096c9ce9e86b3c039a3efb5bf0
MargaridaRPSilva <up201606214@g.uporto.pt>
2018-05-27 11:34:44 +0100

    Completed int8

--- queries/int8.sql
+++ queries/int8.sql
@@ -4,8 +4,8 @@
 
 --Users Responsivos a Mensagens
 
-select * from message 
-	where sender = 1 or receiver = 1
-	order by date asc; 
-	--receiver in (select distinct receiver from message where sender = 1);
+select sender, time(0.5 + avg(timeElapsed)) as averageTimeResponse from 
+(select M1.sender as sender,  julianday(M1.date) - julianday(M2.date) as timeElapsed from message M1 join message M2 
+where ( M1.id > M2.id and M1.context = M2.context) group by M1.id having (M2.id = max(M2.id) and M2.sender <> M1.sender))
+group by sender order by avg(timeElapsed);
 
