8ea3db59d11a1f02156631618dd04323852a90b5
César Medeiros <cesar.mnm@gmail.com>
2018-05-27 16:18:33 +0100

    Int10

--- queries/int10.sql
+++ queries/int10.sql
@@ -7,6 +7,6 @@
 select distinct request.id as ID, request.bookTitle as Book, request.requester, sharing.sender
 	from request, sharing join book join bookItem join bookAuthor join Author
 	where isFulfilled = 1 and
-	(title LIKE request.bookTitle or authorName LIKE author.name)
+	(title LIKE ('%' || request.bookTitle || '%') or authorName LIKE ('%' || author.name || '%'))
 	and request.requester = sharing.receiver
 	order by sharing.startDate LIMIT 1;
