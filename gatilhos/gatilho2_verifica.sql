b11c1e91ea142a48a71a23f454c0577798da1991
Unknown <up201605339@fe.up.pt>
2018-05-27 23:39:19 +0100

    removed id from gatilho 2 verifica

--- gatilhos/gatilho2_verifica.sql
+++ gatilhos/gatilho2_verifica.sql
@@ -2,10 +2,10 @@ PRAGMA foreign_keys=ON;
 
 SELECT * FROM MESSAGE;
 
-INSERT INTO MESSAGE VALUES (10, '2018/06/20', 'Hello, nice book you have!', 3, 4, 2);
+INSERT INTO MESSAGE (date, body, receiver, sender, context) VALUES ('2018/06/20 20:23:32', 'Hello, nice book you have!', 3, 4, 2);
 
 SELECT * FROM MESSAGE; 
 
-INSERT INTO MESSAGE VALUES (11, '2018/07/14', 'Good Morning! How you doing?', 8, 2, 1);
+INSERT INTO MESSAGE (date, body, receiver, sender, context)  VALUES ('2018/07/14', 'Good Morning! How you doing?', 8, 2, 1);
 
 SELECT * FROM MESSAGE;
